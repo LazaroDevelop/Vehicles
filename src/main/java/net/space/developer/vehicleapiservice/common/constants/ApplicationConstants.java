5a3d15082d64e4d7e1e3c4d6dbd3a42d1334d9b0
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-08 10:31:41 -0400

    add application constants for define common constants

---
+++ src/main/java/net/space/developer/vehicleapiservice/common/constants/ApplicationConstants.java
@@ -0,0 +1,15 @@
+package net.space.developer.vehicleapiservice.common.constants;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Application constants class to manage utility variables and methods
+ *
+ * @author Lazaro Noel Guerra Medina
+ * @since 2025-05-08
+ */
+
+@UtilityClass
+public class ApplicationConstants {
+    public final String  VEHICLE_TABLE_NAME = "T_VEHICLE";
+}
