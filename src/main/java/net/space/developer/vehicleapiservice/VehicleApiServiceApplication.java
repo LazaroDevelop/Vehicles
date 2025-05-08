a199cddab2199c6a10412c92fec1796eed447891
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-08 09:39:55 -0400

    initial commit

---
+++ src/main/java/net/space/developer/vehicleapiservice/VehicleApiServiceApplication.java
@@ -0,0 +1,13 @@
+package net.space.developer.vehicleapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleApiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/space/developer/vehicleapiservice/VehicleApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.space.developer.vehicleapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
