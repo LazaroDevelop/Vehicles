b9f04619f14a423d7c8fc833e02431533bd9d9d9
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-08 13:22:53 -0400

    add vehicle model data transfer object

---
+++ src/main/java/net/space/developer/vehicleapiservice/models/VehicleModel.java
@@ -0,0 +1,36 @@
+package net.space.developer.vehicleapiservice.models;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static net.space.developer.vehicleapiservice.common.constants.ApplicationConstants.*;
+
+/**
+ * Vehicle model DTO class
+ *
+ * @author Lazaro Noel Guerra Medina
+ * @since 2025-05-08
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VehicleModel {
+
+    private Long id;
+
+    @NotNull(message = VEHICLE_REGISTRATION_NOT_NULL)
+    @NotBlank(message = VEHICLE_REGISTRATION_NOT_EMPTY)
+    private String vehicleRegistration;
+
+    @NotNull(message = VIN_NOT_NULL)
+    @NotBlank(message = VIN_NOT_EMPTY)
+    private String vehicleIdentificationNumber;
+
+}
