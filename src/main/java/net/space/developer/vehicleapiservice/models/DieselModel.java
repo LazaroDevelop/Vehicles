e89c6b1411018e607432d58e7eefb11e8e90b172
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-08 13:23:00 -0400

    add diesel vehicle model data transfer object

---
+++ src/main/java/net/space/developer/vehicleapiservice/models/DieselModel.java
@@ -0,0 +1,26 @@
+package net.space.developer.vehicleapiservice.models;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import net.space.developer.vehicleapiservice.enums.diesel.InjectionType;
+
+/**
+ * Diesel vehicle model DTO class
+ *
+ * @author Lazaro Noel Guerra Medina
+ * @since 2025-05-08
+ */
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class DieselModel extends VehicleModel{
+
+    private InjectionType injectionType;
+
+    public DieselModel(Long id, String vehicleRegistration, String vehicleIdentificationNumber, InjectionType injectionType){
+        super(id, vehicleRegistration, vehicleIdentificationNumber);
+        this.injectionType = injectionType;
+    }
+}
