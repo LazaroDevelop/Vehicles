0a360362f23174ccaa45f619af4708ac5fd47ecb
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-09 15:51:57 -0400

    update application constants

--- src/main/java/net/space/developer/vehicleapiservice/common/constants/ApplicationConstants.java
+++ src/main/java/net/space/developer/vehicleapiservice/common/constants/ApplicationConstants.java
@@ -12,6 +12,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ApplicationConstants {
 
+    // Entities related messages
     public final String VEHICLE_TABLE_NAME = "T_VEHICLE";
     public final String VEHICLE_ID = "VEHICLE_ID";
     public final String VEHICLE_TYPE = "VEHICLE_TYPE";
@@ -32,8 +33,14 @@ public class ApplicationConstants {
     public final String VIN_NOT_NULL = "Vehicle Identification Number field cannot be null";
     public final String VIN_NOT_EMPTY = "Vehicle Identification Number field cannot be empty";
 
+    // Custom exception messages
     public final String VEHICLE_NOT_FOUND_MESSAGE = "Vehicle was not found";
     public final String VEHICLE_ALREADY_EXISTS_MESSAGE = "This vehicle is already registered";
     public final String VEHICLE_INVALID_MESSAGE = "Invalid vehicle registration";
     public final String VEHICLE_INVALID_WITH_BODY = "Invalid vehicle registration for vehicle model {0}";
+    public final String VEHICLE_CONVERSION_FAILED_MESSAGE = "Invalid conversion from electrical model to gasoline model";
+
+    //Routes
+    public final String API_BASE_URL = "/api/v1/vehicles";
+
 }
