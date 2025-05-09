1df0943a35b5ec7c28527b07bbcc9e840933e7bc
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-09 15:52:20 -0400

    add electrical repository

--- src/main/java/net/space/developer/vehicleapiservice/repository/ElectricalRepository.java
+++ src/main/java/net/space/developer/vehicleapiservice/repository/ElectricalRepository.java
@@ -4,6 +4,13 @@ import net.space.developer.vehicleapiservice.domain.ElectricalVehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Electrical repository class
+ *
+ * @author Lazaro Noel Guerra Medina
+ * @since 2025-05-09
+ */
+
 @Repository
 public interface ElectricalRepository extends JpaRepository<ElectricalVehicle, Long> {
 }
