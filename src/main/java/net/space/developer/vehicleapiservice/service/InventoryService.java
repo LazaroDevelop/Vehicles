6ffdb3b43d2fe69d1d0e3ed775202ae4382a5e4e
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-09 11:06:00 -0400

    add inventory service

---
+++ src/main/java/net/space/developer/vehicleapiservice/service/InventoryService.java
@@ -0,0 +1,82 @@
+package net.space.developer.vehicleapiservice.service;
+
+import net.space.developer.vehicleapiservice.enums.VehicleType;
+import net.space.developer.vehicleapiservice.model.VehicleModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ * Inventory service interface with all the uses cases signatures
+ *
+ * @author Lazaro Noel Guerra Medina
+ * @since 2025-05-08
+ */
+
+public interface InventoryService {
+    /**
+     * Retrieve all the vehicles in the inventory
+     *
+     * @return a {@link List} of {@link VehicleModel}
+     */
+    List<VehicleModel> getAllVehicles();
+
+    /**
+     * Retrieve all the vehicles in the inventory with pagination and sorting
+     *
+     * @param pageable the page information to paginate the result
+     * @return a {@link Page} of {@link VehicleModel}
+     */
+    Page<VehicleModel> getAllVehicles(Pageable pageable);
+
+    /**
+     * Retrieve all the vehicles in the inventory by vehicle type
+     *
+     * @param type the vehicle type
+     * @return a {@link List} of {@link VehicleModel}
+     */
+    List<VehicleModel> getVehiclesByType(VehicleType type);
+
+    /**
+     * Retrieve all the vehicles in the inventory
+     *
+     * @param type the vehicle type
+     * @param pageable the page information to paginate the result
+     * @return a {@link Page} of {@link VehicleModel}
+     */
+    Page<VehicleModel> getVehiclesByType(VehicleType type, Pageable pageable);
+
+    /**
+     * Get a specific vehicle by identifier
+     *
+     * @param id the vehicle identifier
+     * @return the found {@link VehicleModel}
+     */
+    VehicleModel getVehicleById(long id);
+
+    /**
+     * Register a new vehicle
+     *
+     * @param vehicleModel the vehicle information
+     * @return the created or registered vehicle
+     */
+    VehicleModel createVehicle(VehicleModel vehicleModel);
+
+    /**
+     * Update an existing vehicle by identifier
+     *
+     * @param vehicleModel the new vehicle information
+     * @param id the given identifier
+     * @return the updated {@link VehicleModel}
+     */
+    VehicleModel updateVehicle(VehicleModel vehicleModel, long id);
+
+    /**
+     * Drop or delete an existing vehicle by identifier
+     *
+     * @param id the vehicle identifier
+     * @return true if was deleted, false in other case
+     */
+    boolean deleteVehicle(long id);
+}
