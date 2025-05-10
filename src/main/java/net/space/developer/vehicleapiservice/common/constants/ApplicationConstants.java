117b04af385861d0e39632b3efcccded6925b516
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-10 12:00:39 -0400

    do refactor in application constants class

--- src/main/java/net/space/developer/vehicleapiservice/common/constants/ApplicationConstants.java
+++ src/main/java/net/space/developer/vehicleapiservice/common/constants/ApplicationConstants.java
@@ -1,7 +1,5 @@
 package net.space.developer.vehicleapiservice.common.constants;
 
-import lombok.experimental.UtilityClass;
-
 /**
  * Application constants class to manage utility variables and methods
  *
@@ -9,38 +7,36 @@ import lombok.experimental.UtilityClass;
  * @since 2025-05-08
  */
 
-@UtilityClass
-public class ApplicationConstants {
+public final class ApplicationConstants {
 
     // Entities related messages
-    public final String VEHICLE_TABLE_NAME = "T_VEHICLE";
-    public final String VEHICLE_ID = "VEHICLE_ID";
-    public final String VEHICLE_TYPE = "VEHICLE_TYPE";
-    public final String VEHICLE_REGISTRATION = "VEHICLE_REGISTRATION";
-    public final String VEHICLE_IDENTIFICATION_NUMBER = "VEHICLE_IDENTIFICATION_NUMBER";
-
-    public final String GASOLINE_TYPE = "GASOLINE_TYPE";
+    public static final  String VEHICLE_TABLE_NAME = "T_VEHICLE";
+    public static final  String VEHICLE_ID = "VEHICLE_ID";
+    public static final  String VEHICLE_REGISTRATION = "VEHICLE_REGISTRATION";
+    public static final  String VEHICLE_IDENTIFICATION_NUMBER = "VEHICLE_IDENTIFICATION_NUMBER";
 
     // Discriminator values
-    public final String DISCRIMINATOR_NAME = "vehicle_type";
-    public final String DISCRIMINATOR_DIESEL = "DIESEL";
-    public final String DISCRIMINATOR_GASOLINE = "GASOLINE";
-    public final String DISCRIMINATOR_ELECTRICAL = "ELECTRICAL";
+    public static final String DISCRIMINATOR_NAME = "_type";
+    public static final String DISCRIMINATOR_DIESEL = "_diesel";
+    public static final String DISCRIMINATOR_GASOLINE = "_gasoline";
+    public static final String DISCRIMINATOR_ELECTRICAL = "_electrical";
 
     // Validation messages
-    public final String VEHICLE_REGISTRATION_NOT_NULL = "Vehicle Registration field cannot be null";
-    public final String VEHICLE_REGISTRATION_NOT_EMPTY = "Vehicle Registration field cannot be empty";
-    public final String VIN_NOT_NULL = "Vehicle Identification Number field cannot be null";
-    public final String VIN_NOT_EMPTY = "Vehicle Identification Number field cannot be empty";
+    public static final String VEHICLE_REGISTRATION_NOT_NULL = "Vehicle Registration field cannot be null";
+    public static final String VEHICLE_REGISTRATION_NOT_EMPTY = "Vehicle Registration field cannot be empty";
+    public static final String VIN_NOT_NULL = "Vehicle Identification Number field cannot be null";
+    public static final String VIN_NOT_EMPTY = "Vehicle Identification Number field cannot be empty";
 
     // Custom exception messages
-    public final String VEHICLE_NOT_FOUND_MESSAGE = "Vehicle was not found";
-    public final String VEHICLE_ALREADY_EXISTS_MESSAGE = "This vehicle is already registered";
-    public final String VEHICLE_INVALID_MESSAGE = "Invalid vehicle registration";
-    public final String VEHICLE_INVALID_WITH_BODY = "Invalid vehicle registration for vehicle model {0}";
-    public final String VEHICLE_CONVERSION_FAILED_MESSAGE = "Invalid conversion from electrical model to gasoline model";
+    public static final String VEHICLE_NOT_FOUND_MESSAGE = "Vehicle was not found";
+    public static final String VEHICLE_ALREADY_EXISTS_MESSAGE = "This vehicle is already registered";
+    public static final String VEHICLE_INVALID_MESSAGE = "Invalid vehicle registration";
+    public static final String VEHICLE_INVALID_WITH_BODY = "Invalid vehicle registration for vehicle model {0}";
+    public static final String VEHICLE_CONVERSION_FAILED_MESSAGE = "Invalid conversion from electrical model to gasoline model";
 
     //Routes
-    public final String API_BASE_URL = "/api/v1/vehicles";
+    public static final String API_BASE_URL = "/api/v1/vehicles";
+
+    private ApplicationConstants(){}
 
 }
